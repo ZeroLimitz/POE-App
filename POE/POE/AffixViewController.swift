fbb970f08dff660f3d096cf9eb1ce87420ae8813
Brandon Kueker <brandon.kueker@red8interactive.com>
2015-06-18 12:41:05 -0500

    added project

---
+++ POE/POE/AffixViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AffixViewController.swift
+//  POE
+//
+//  Created by Brandon Kueker on 6/18/15.
+//  Copyright (c) 2015 Brandon Kueker. All rights reserved.
+//
+
+import UIKit
+
+class AffixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ POE/POE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POE
+//
+//  Created by Brandon Kueker on 6/18/15.
+//  Copyright (c) 2015 Brandon Kueker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
